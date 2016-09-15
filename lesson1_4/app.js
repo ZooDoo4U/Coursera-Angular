8ac28ba91d9c7531b324c9f02b5be5f0d8a51d54
Dan Doris <dano2k3@hotmail.com>
2016-09-15 11:14:08 -0700

    updated sample

--- lesson1_4/app.js
+++ lesson1_4/app.js
@@ -3,10 +3,29 @@
     'use strict';
 //    alert("hello");
       
-    var app = angular.module("myFirstApp",[]);
-    app.controller( "myFirstController", function($scope) {
-    $scope.name = "Dano";
-//    $scope.sayHello  = function() { return "Hello " + $scope.name;}
-});
+    var app = angular.module("nameCalculator",[]);
+    app.controller( "nameCalculatorController", function($scope) 
+    {
+        $scope.name="";
+        $scope.nameTotalValue=0;
+        $scope.displayNameValue   = function()
+        {
+            var tmpVal  = calculateNameValue($scope.name); 
+            $scope.nameTotalValue = tmpVal;
+        }     
+    });
     
 })(); 
+
+
+function calculateNameValue(theString) 
+{
+    var tmpVal =0  ; 
+    for( var i=0; i < theString.length;++i)
+    {
+        tmpVal  += (theString.charCodeAt(i));
+    }
+    return tmpVal; 
+
+
+}
